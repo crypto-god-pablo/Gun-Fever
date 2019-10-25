845c7d04f801cf172385121541648fc0e19a7341
Cherie Hong <chong3@hwemail.com>
2019-10-24 21:38:30 -0700

    Edited main.js and added index.html
    Coded functions that check if user clicked single or multiplayer in main.js. Also added basic index.html file.

--- main.js
+++ main.js
@@ -1,19 +1,61 @@
 /*the game which is currently running (or was running, but is now over); this is
 null at the beginning of the program*/
 Game currentGame;
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+//button for singleplayer
+var button1 = {
+  x:250,
+  y:350,
+  width:200,
+  height:100
+};
+//button for multiplayer
+var button2 = {
+  x:250,
+  y:175,
+  width:200,
+  height:100
+};
 
 //draws the menu screen
-void drawMenu() {}
+void drawMenu() {
+}
 
-/*returns true when the mouse is clicked; NOTE: I believe there is a better to
-way to code this, using an Event*/
-bool mouseClick() {}
+//Function to check whether a point is inside a rectangle
+function isInside(pos, rect){
+    return pos.x > rect.x && pos.x < rect.x+rect.width && pos.y < rect.y+rect.height && pos.y > rect.y
+}
+
+/*Checks if user clicked singleplayer or multiplayer*/
+canvas.addEventListener('click', function(evt) {
+    var mousePos = getMousePos(canvas, evt);
+    if (isInside(mousePos, button1)) {
+        singleplayerClicked();
+    }
+    else{
+        multiplayerClicked();
+    }
+}, false);
 
 //returns true if the mouse most recently clicked the singleplayer button
-bool singpleplayerClicked() {}
+bool singpleplayerClicked() {
+  returns true;
+}
 
 //return true if the mouse most recently clicked the multiplayer button
-bool multiplayerClicked() {}
+bool multiplayerClicked() {
+  return true;
+}
+
+//function to get mouse position
+function getMousePos(canvas, event) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top
+    };
+}
 
 //the main method which will loop continuously
 void main() {
